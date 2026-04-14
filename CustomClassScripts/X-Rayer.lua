6063146b1aedebeb8dc3549d6d4ce08134fafd14
TomCodes <Thomasmcguire64@Gmail.com>
2026-04-14 10:41:09 +1000

    Xrayer CD 5 secs

--- CustomClassScripts/X-Rayer.lua
+++ CustomClassScripts/X-Rayer.lua
@@ -2,7 +2,7 @@ local classname = "X-Rayer"
 
 ListenToEvent("AbilityKeyPressed_OnClient", function(playerActor)
 	if playerActor.CustomClassString == classname then
-		playerActor:startAbilityCooldown(35.0)
+		playerActor:startAbilityCooldown(45.0)
 		
 		local customers = GetAllActorsOfClass("AI_Customer")
 		local employees = GetAllActorsOfClass("AI_Employee")
@@ -21,7 +21,7 @@ ListenToEvent("AbilityKeyPressed_OnClient", function(playerActor)
 		
 		PlaySound(playerActor, "xrayeractivate.wav", 0.75)
 		GetGameState():ShowLuaImage("xrayersight", "xrayerblue.png", 0, 0, 10.0, 10000, 10000)
-		SetTimer(7.0, "XRayVisionOff", playerActor)
+		SetTimer(5.0, "XRayVisionOff", playerActor)
 	end
 end)
 
