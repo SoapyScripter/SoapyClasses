11e752f316a50906d98e2b3d63e2359c91c0b491
SoapyScripter <152108533+SoapyScripter@users.noreply.github.com>
2026-04-17 18:46:19 -0500

    tycoon

---
+++ CustomClassScripts/Tycoon.lua
@@ -0,0 +1,33 @@
+local classname = "Tycoon"
+
+ListenToEvent("AbilityKeyPressed_OnClient", function(playerActor)
+    if playerActor.CustomClassString == classname then
+        playerActor:startAbilityCooldown(45.0)
+
+        playerActor:AbilitySV()
+    end
+end)
+
+ListenToEvent("AbilitySV", function(playerActor)
+    if playerActor.CustomClassString == classname then
+        playerActor.ActionComponent:setStealMulti(1.15,3.0)
+    end
+end)
+
+ListenToEvent("RoundTick", function()
+	for i, player in ipairs(GetPlayerChars()) do
+		if player.CustomClassString == classname then
+            local moneyPercentage = GetGameState().savedMoney/GetGameState().requiredSavedMoney
+
+            player.ActionComponent:SlowDownTimeSV(1 + (moneyPercentage/2))
+		end
+	end
+end)
+
+ListenToEvent("PreReceiveDamage", function(target, source, damage)
+	if source then
+		if source.CustomClassString == classname then
+			target.HP = target.HP - math.ceil(damage * ((GetGameState().savedMoney/GetGameState().requiredSavedMoney)/2))
+		end
+	end
+end)
