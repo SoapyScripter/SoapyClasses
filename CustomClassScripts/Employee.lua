21c613eb57c2c8ec1f1153f2e96eaf2876559199
SoapyScripter <152108533+SoapyScripter@users.noreply.github.com>
2026-05-26 14:39:39 -0500

    e

---
+++ CustomClassScripts/Employee.lua
@@ -0,0 +1,40 @@
+local classname = "Employee"
+
+local function GetDistance(actor1, actor2)
+    local a = actor1:GetActorLocation()
+    local b = actor2:GetActorLocation()
+    local dx, dy, dz = a.X-b.X, a.Y-b.Y, a.Z-b.Z
+    return math.sqrt(dx*dx + dy*dy + dz*dz)
+end
+
+ListenToEvent("AbilityKeyPressed_OnClient", function(playerActor)
+    if playerActor.CustomClassString == classname then
+        playerActor:startAbilityCooldown(30.0)
+
+        playerActor:AbilitySV()
+    end
+end)
+
+ListenToEvent("AbilitySV", function(playerActor)
+    if playerActor.CustomClassString == classname then
+        for i, pc in ipairs(GetAllActorsOfClass("HackablePC")) do
+            if GetDistance(pc,playerActor) <= 1000 then
+                if GetGameState().requiredSavedMoney > 1000 then
+                    GetGameState().requiredSavedMoney = GetGameState().requiredSavedMoney - 1000
+                    GetGameState().savedMoney = GetGameState().savedMoney + 1000
+                    GetGameState():SpawnLuaPingSV("employeeembezzlement.png", pc:GetActorLocation())
+                end
+            end
+        end
+    end
+end)
+
+ListenToEvent("RoundTick", function()
+    for i, player in ipairs(GetPlayerChars()) do
+        if player.CustomClassString == classname then
+            if player.CurrentZone == 1 then
+                player.ActionComponent:setStealMulti(1.5,0.05)
+            end
+        end
+    end
+end)
---
+++ CustomClassScripts/NikolaTesla.lua
@@ -0,0 +1,36 @@
+local classname = "NikolaTesla"
+
+local function GetDistance(actor1, actor2)
+    local a = actor1:GetActorLocation()
+    local b = actor2:GetActorLocation()
+    local dx, dy, dz = a.X-b.X, a.Y-b.Y, a.Z-b.Z
+    return math.sqrt(dx*dx + dy*dy + dz*dz)
+end
+
+ListenToEvent("AbilityKeyPressed_OnClient", function(playerActor)
+    if playerActor.CustomClassString == classname then
+        playerActor:startAbilityCooldown(30.0)
+
+        playerActor:AbilitySV()
+    end
+end)
+
+ListenToEvent("AbilitySV", function(playerActor)
+    if playerActor.CustomClassString == classname then
+        local closest = GetClosestActor("CCTV", playerActor:GetActorLocation())
+
+		if closest and GetDistance(playerActor, closest) <= 500 then
+            AddActorTag(closest, "TeslaTurret")
+            LogMessage("ada")
+        end
+    end
+end)
+
+ListenToEvent("PreReceiveDamage", function(targetActor, sourceActor, damage)
+    if sourceActor then
+        if sourceActor.CustomClassString == classname then
+            targetActor.HP = targetActor.HP + math.ceil(damage/2)
+            targetActor:TaseredSV(1.0)
+        end
+    end
+end)
